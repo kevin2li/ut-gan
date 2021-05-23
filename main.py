712ad2e6c30f0c336a2d92dd89fd70675d2bcf33
kevin2li <kevin2li@qq.com>
2021-05-23 21:35:12 +0800

    add trainer

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 import torch
 import torch.optim as optim
 from dataloaders import get_mnist_dataloaders, get_lsun_dataloader
-from models import Generator, Discriminator
-from training import Trainer
+from src.models import UNet, ZhuNet
+from .trainer import Trainer
 
 data_loader, _ = get_mnist_dataloaders(batch_size=64)
 img_size = (32, 32, 1)
 
-generator = Generator(img_size=img_size, latent_dim=100, dim=16)
-discriminator = Discriminator(img_size=img_size, dim=16)
+generator = UNet(3, 2)
+discriminator = ZhuNet()
 
 print(generator)
 print(discriminator)
@@ -24,9 +24,9 @@ D_optimizer = optim.Adam(discriminator.parameters(), lr=lr, betas=betas)
 epochs = 200
 trainer = Trainer(generator, discriminator, G_optimizer, D_optimizer,
                   use_cuda=torch.cuda.is_available())
-trainer.train(data_loader, epochs, save_training_gif=True)
+trainer.train(data_loader, epochs, save_training_gif=False)
 
 # Save models
-name = 'mnist_model'
-torch.save(trainer.G.state_dict(), './gen_' + name + '.pt')
-torch.save(trainer.D.state_dict(), './dis_' + name + '.pt')
+# name = 'mnist_model'
+# torch.save(trainer.G.state_dict(), './gen_' + name + '.pt')
+# torch.save(trainer.D.state_dict(), './dis_' + name + '.pt')
